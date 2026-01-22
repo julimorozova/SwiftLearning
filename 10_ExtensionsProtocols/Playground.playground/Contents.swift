1471904b68c7f49bfb23618db0f612504daa213d
Iuliia Morozova <yulii.morozova@gmail.com>
2026-01-22 11:22:41 +0100

    add protocols

--- 10_ExtensionsProtocols/Playground.playground/Contents.swift
+++ 10_ExtensionsProtocols/Playground.playground/Contents.swift
@@ -1,3 +1,138 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+//1 - Расширение String
+//Добавь в String функцию isPalindrome(), которая проверяет, является ли строка палиндромом.
+
+extension String {
+    func isPalindrome() -> Bool {
+        var str = ""
+
+        for char in self {
+            if char.isLetter {
+                str.append(char)
+            }
+        }
+
+        return str == String(str.reversed())
+    }
+}
+
+//2 - Расширение Int
+//Добавь метод squared() для Int, который возвращает квадрат числа.
+
+extension Int {
+    func squared() {
+        self * self
+    }
+}
+
+//3 - Класс "Person"
+//Создай класс Person с полями name и age.
+//Добавь метод introduce() в расширении, который выводит в консоль: "Меня зовут name, мне age лет".
+
+class Person {
+    var name: String
+    var age: Int
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+}
+extension Person {
+    func introduce() {
+        print("Меня зовут \(name), мне \(age) лет")
+    }
+}
+
+//4 - Протокол "Drawable"
+//Определи протокол Drawable с методом draw().
+//Реализуй его в классе Circle и Square, чтобы они выводили в консоль описание: "Рисую круг радиусом 10" или "Рисую квадрат со стороной 5".
+
+protocol Drawable {
+    func draw()
+}
+
+class Circle: Drawable {
+    func draw() {
+        print("Рисую круг радиусом 10")
+    }
+    
+
+}
+
+class Square: Drawable {
+    func draw() {
+        print("Рисую квадрат со стороной 5")
+    }
+}
+
+
+//5 - Протокол "Calculatable"
+//Определи протокол с методом calculate(a:b:) -> Int.
+//Реализуй его в структурах Adder, Multiplier
+
+protocol Calculatable {
+    func calculate(a: Int, b:Int ) -> Int
+}
+
+struct Adder: Calculatable {
+    func calculate(a: Int, b: Int) -> Int {
+        a + b
+    }
+}
+
+struct Multiplier: Calculatable {
+    func calculate(a: Int, b: Int) -> Int {
+        a * b
+    }
+}
+//6 - Протокол "Printable"
+//Определи протокол с методом printInfo().
+//Реализуй в Car (пусть выводит марку) и Phone (пусть выводит модель).
+
+protocol Printable {
+    func printInfo()
+}
+
+class Car: Printable {
+    var brand: String
+
+    init(brand: String) {
+        self.brand = brand
+    }
+
+    func printInfo() {
+        print("Марка автомобиля: \(brand)")
+    }
+}
+
+class Phone: Printable {
+    var model: String
+
+    init(model: String) {
+        self.model = model
+    }
+
+    func printInfo() {
+        print("Модель телефона: \(model)")
+    }
+}
+
+//7 - Протокол "Named"
+//Создай протокол Named с одним свойством name: String.
+//Реализуй его в классе Dog и структуре Book.
+
+protocol Named {
+    var name: String { get set }
+}
+
+class Dog: Named {
+    var name: String
+    init(name: String) {
+        self.name = name
+    }
+}
+
+struct Book: Named {
+    var name: String
+}
